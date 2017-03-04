4c3e2a626e0477290cb5ec542de5280895a107e3
Travis Merkel <tmerkel@live.com>
2017-03-04 13:09:25 -0800

    Initail checkin

---
+++ src/services/I520.Fremont.Services.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace I520.Services.Project.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
